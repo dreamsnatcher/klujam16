363cc9aa4b828621d51f489a01ca01ce24c90ee5
Xifiggam <xifiggam@gmx.at>
2016-04-15 22:12:59 +0200

    Gamplay Screen useless stuff removed

--- core/src/at/klujam/game/screens/GameplayScreen.java
+++ core/src/at/klujam/game/screens/GameplayScreen.java
@@ -20,23 +20,12 @@ public class GameplayScreen extends ScreenAdapter {
     final SpriteBatch batch;
     final OrthographicCamera cam;
     public Game parentGame;
-
-    Texture backgroundImage;
-    BitmapFont menuFont;
     World world;
-    String[] menuStrings = {"Play", "Credits", "Exit"};
-    int currentMenuItem = 0;
-
-    float offsetLeft = Game.GAME_WIDTH / 8, offsetTop = Game.GAME_WIDTH / 8, offsetY = Game.GAME_HEIGHT / 8;
 
 
     public GameplayScreen(Game game) {
         this.parentGame = game;
         this.world = new World(this);
-
-        backgroundImage = parentGame.getAssetManager().get("menu/menu_background.jpg");
-        menuFont = parentGame.getAssetManager().get("menu/Ravie_72.fnt");
-        menuFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         // Create camera that projects the game onto the actual screen size.
         cam = new OrthographicCamera(Game.GAME_WIDTH, Game.GAME_HEIGHT);
 
