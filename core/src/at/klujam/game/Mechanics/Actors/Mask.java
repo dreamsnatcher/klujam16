0995e83decbd2114bcc60325ed739614ef108310
Bone <l.knoch@gmx.at>
2016-04-16 21:37:41 +0200

    sdf

--- core/src/at/klujam/game/Mechanics/Actors/Mask.java
+++ core/src/at/klujam/game/Mechanics/Actors/Mask.java
@@ -1,6 +1,6 @@
 package at.klujam.game.Mechanics.Actors;
 
-import at.klujam.game.Mechanics.Entities.GameObject;
+import at.klujam.game.Mechanics.Entities.F_Entity;
 import at.klujam.game.Mechanics.FightWorld;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -9,14 +9,14 @@ import com.badlogic.gdx.math.Vector2;
 /**
  * Created by lknoch on 16.04.16.
  */
-public class Mask extends GameObject{
+public class Mask extends F_Entity{
     private final Texture texture;
     private final Vector2 position;
     private final Vector2 dimension;
     private final FightWorld fightWorld;
 
     public Mask(Vector2 position, Vector2 dimension, FightWorld fightWorld) {
-        super(position,dimension);
+        super(position,fightWorld);
         this.position = position;
         this.dimension = dimension;
         this.fightWorld = fightWorld;
