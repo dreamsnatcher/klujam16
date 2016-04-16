eb34edb4ba4c3d2eab2357033b64b0d69d39ee81
Lorenz Schmoliner <l.schmoliner@gmx.net>
2016-04-16 14:04:03 +0200

    gameobjects.create

--- core/src/at/klujam/game/Game.java
+++ core/src/at/klujam/game/Game.java
@@ -52,6 +52,7 @@ public class Game extends ApplicationAdapter {
 		assMan.load("gameplay/movingAnimation_Down.png", Texture.class);
 		assMan.load("buttons/button_wood_active.png", Texture.class);
 		assMan.load("buttons/button_wood_idle.png", Texture.class);
+		assMan.load("walls/wall_line_1.png", Texture.class);
         //assMan.load("fonts/font.fnt", BitmapFont.class);
 	}
 
--- core/src/at/klujam/game/screens/DungeonScreen.java
+++ core/src/at/klujam/game/screens/DungeonScreen.java
@@ -1,13 +1,10 @@
 package at.klujam.game.screens;
 
 import at.klujam.game.Game;
-import at.klujam.game.util.Collisions;
+import at.klujam.game.util.GameObjects;
 import at.klujam.game.util.Constants;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 
 /**
  * Created by lknoch on 15.04.16.
@@ -22,7 +19,7 @@ public class DungeonScreen extends GameplayScreen {
         this.level = level;
         this.tMap = (new TmxMapLoader()).load("level/" + level + ".tmx");
 
-        Collisions.create(world, tMap, unitScale);
+        GameObjects.create(world, tMap, unitScale);
     }
 
     public DungeonScreen(Game game) {
