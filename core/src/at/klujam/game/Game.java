7176a3559beb7771c14ca2589481c091f5a3d5f5
Bone <l.knoch@gmx.at>
2016-04-16 23:12:36 +0200

    animation in VeitScreen

--- core/src/at/klujam/game/Game.java
+++ core/src/at/klujam/game/Game.java
@@ -117,6 +117,14 @@ public class Game extends ApplicationAdapter {
 		assMan.load("gameplay/selected1.png", Texture.class);
 		assMan.load("gameplay/selected2.png", Texture.class);
 
+		assMan.load("gameplay/gobo_2_big_000.png", Texture.class);
+		assMan.load("gameplay/gobo_2_big_001.png", Texture.class);
+		assMan.load("gameplay/gobo_2_big_002.png", Texture.class);
+
+		assMan.load("gameplay/gobo_1_big_000.png", Texture.class);
+		assMan.load("gameplay/gobo_1_big_001.png", Texture.class);
+		assMan.load("gameplay/gobo_1_big_002.png", Texture.class);
+
 		//assMan.load("fonts/font.fnt", BitmapFont.class);
 	}
 
--- core/src/at/klujam/game/Mechanics/Entities/F_Entity.java
+++ core/src/at/klujam/game/Mechanics/Entities/F_Entity.java
@@ -6,9 +6,8 @@ import at.klujam.game.Mechanics.States.F_Dead;
 import at.klujam.game.Mechanics.States.F_State;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
@@ -21,6 +20,7 @@ import java.util.List;
  */
 public abstract class F_Entity{
 
+    protected Animation animation;
     private final GlyphLayout glyphLayout;
     public int hitpoints = 100;
     public float resistence = 0;
@@ -42,17 +42,20 @@ public abstract class F_Entity{
     private float showStateDuration = 0;
     private Color stateColor;
     private String stateString;
+    private float animate =0;
+    private float nextAnimation = MathUtils.random(0,15);
 
 
     public F_Entity(Vector2 position, FightWorld world) {
         this.position = position;
         this.world = world;
         states = new Array<F_State>();
+
         selector1Textur = world.fightingSceneScreen.parentGame.getAssMan().get("gameplay/selected1.png");
         selector2Textur = world.fightingSceneScreen.parentGame.getAssMan().get("gameplay/selected2.png");
         numberFont = world.fightingSceneScreen.parentGame.getAssMan().get("fonts/celtic_small.fnt");
         if(this instanceof F_Enemy) {
-            numberFont = world.fightingSceneScreen.parentGame.getAssMan().get("fonts/celtic_even_small.fnt");
+            numberFont = world.fightingSceneScreen.parentGame.getAssMan().get("fonts/celtic_even_smaller.fnt");
         }
 
         glyphLayout = new GlyphLayout();
@@ -77,9 +80,24 @@ public abstract class F_Entity{
         }
     };
 
+    public void doAnimation(){
+            animate = 0;
+    }
     public void render(float delta, SpriteBatch spriteBatch) {
 
-        spriteBatch.draw(texture, position.x, position.y);
+        if(nextAnimation <= 0){
+            doAnimation();
+            nextAnimation = MathUtils.random(3f,20f);
+        }
+        nextAnimation-=delta;
+
+        if(animation!=null && animate <= animation.getAnimationDuration() ){
+            TextureRegion texture = animation.getKeyFrame(animate,true);
+            spriteBatch.draw(texture, position.x, position.y);
+            animate+=delta;
+        }else{
+            spriteBatch.draw(texture, position.x, position.y);
+        }
         if(selectedByTwo){
             spriteBatch.draw(selector1Textur, position.x + texture.getWidth()/2 + 9, position.y + texture.getHeight());
 
@@ -117,7 +135,7 @@ public abstract class F_Entity{
         numberFont.draw(spriteBatch, armorString,position.x + (texture.getWidth()/2f) -sumWidth/2f +apOffset,position.y - numberFont.getXHeight() +10);
 
         numberFont.setColor(Color.RED);
-        numberFont.draw(spriteBatch, armorString,position.x + (texture.getWidth()/2f) - sumWidth/2f + atOffset,position.y - numberFont.getXHeight() +10);
+        numberFont.draw(spriteBatch, attack,position.x + (texture.getWidth()/2f) - sumWidth/2f + atOffset,position.y - numberFont.getXHeight() +10);
 
 
         if(showStateDuration>0){
